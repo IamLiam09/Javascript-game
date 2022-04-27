a0c777ab27197fd15ebe92a54353544bb79ebf14
liams <princewill835@gmail.com>
2022-04-26 17:07:20 -0700

    The game is done

--- code.js
+++ code.js
@@ -1,37 +1,62 @@
+var playerscore = 0;
+var computerscore = 0;
 function computerplay(){
     const plays = ["Rock", "Paper", "Scissor"];
     return comp_outcome = plays[Math.floor(Math.random() * plays.length)];
 }
+// User input function here
 function playersselection(){
     const player_select = prompt("Rock, Paper, Scissor  ");
     return player_select;
 }
+// At random the computer plays
 function playround(){
     const computer = computerplay();
     const player = playersselection();
+    var compwin = "You Lose!"
+    var playerwin = "You Win!"
+    var same = "Draw"
     if(computer === "Rock" && player === "Scissor"){
-        console.log("You Lose! Rock beats Scissors")
+        console.log( compwin + " Rock beats Scissors")
+        computerscore++
     } else if (computer === "Paper" && player === "Rock"){
-        console.log("You Lose! Paper beats Rock")
+        console.log( compwin + " Paper beats Rock")
+        computerscore++
     } else if(computer === "Scissor" && player === "Paper"){
-        console.log("You Lose! Scissor beats Paper")
+        console.log( compwin + " Scissor beats Paper")
+        computerscore++
     } else if(player === "Scissor" && computer === "Paper"){
-        console.log("You Win! Scissor beats Paper")
+        console.log(playerwin + " Scissor beats Paper")
+        playerscore++
     } else if (player === "Paper" && computer === "Rock"){
-        console.log("You Win! Paper beats Rock")
+        console.log(playerwin + " Paper beats Rock")
+        playerscore++
     } else if(player === "Rock" && computer === "Scissor"){
-        console.log("You Win! Rock beats Scissors")
+        console.log(playerwin + " Rock beats Scissors")
+        playerscore++
+    } else if (computer === "Paper" && player === "Paper"){
+        console.log(same)
+    } else if (computer === "Rock" && player === "Rock"){
+        console.log(same)
+    } else if (computer === "Scissor" && player === "Scissor"){
+        console.log(same)
     }
 }
-function record(){
-    if(playround === )
+// Break the game if the condition is met
+function Winner(){
+    var Total_score = 5
+    if(playerscore === Total_score){
+        console.log("Congrats, you deafeated a bot");
+    } else if(computerscore === Total_score){
+        console.log("You suck, You lost to a bot");
+    }
 }
-function computer_score{
-
+// Run the game
+function game(){
+    for (i = 0; i < 20; i++){
+        playround()
+        Winner()
+        if(computerscore === 5 || playerscore === 5)break;
+    }
 }
-function player_score{
-
-}
-for (i = 0, i < 5, i++){
-    playround()
-}
+game()
